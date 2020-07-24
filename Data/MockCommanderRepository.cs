cfb9a42348cd46f914a898416ee9edc9297e3575
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-07-24 15:47:50 -0600

    Add mock repository

---
+++ Data/MockCommanderRepository.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using Commander.Models;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepository : ICommanderRepository
+    {
+        public IEnumerable<Command> GetAllCommands()
+        {
+            return new List<Command> {
+                new Command { Id = 0, HowTo = "Boil an eggg", Line = "Boil water", Platform = "Kettle & pan" },
+                new Command { Id = 1, HowTo = "Cut bread", Line = "Get a knife", Platform = "Knife & chopping board" },
+                new Command { Id = 2, HowTo = "Make a cup of tea", Line = "Place a teabag in cup", Platform = "Kettle & cup" },
+            };
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command { Id = id, HowTo = "Boil an eggg", Line = "Boil water", Platform = "Kettle & pan" };
+        }
+    }
+}
